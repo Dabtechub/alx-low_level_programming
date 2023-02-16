5cf7f5b9e1991ecadacd2a2a207b755ce947b0d7
Bismark <bismarkdwamenaakenten@gmail.com>
2023-02-16 07:49:16 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line using puts funtion
+ * Return: 0 (executed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
