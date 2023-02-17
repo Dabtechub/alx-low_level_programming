78767e18cd00c30041eb55a96be28d7533e523f9
Bismark <bismarkdwamenaakenten@gmail.com>
2023-02-17 08:45:40 -0800

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Read Alphabet
+ *
+ * Return: 0 (executed)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar alpha(i)
+		putchar('\n');
+		return (0);
+	}
+}
