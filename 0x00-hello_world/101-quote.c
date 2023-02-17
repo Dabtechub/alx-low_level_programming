79fb402a7ce42a9654a5c53192a45da6d8df934f
Bismark <bismarkdwamenaakenten@gmail.com>
2023-02-16 20:23:31 -0800

    question8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unitstd.h>
+/**
+ * main - C program that prints a line to the standard error
+ * Return 1 (exercuted)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return(1);
