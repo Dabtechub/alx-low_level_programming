ed85eb14645cc7181dffe3f2c794286683401113
Bismark <bismarkdwamenaakenten@gmail.com>
2023-02-21 16:48:44 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,16 @@
 
 /**
  * _isalpha - Checks for alphabet character
- * @C: The caharacter to be checked
+ * @c: The caharacter to be checked
  * Return: 1 for alphabet character or 0 for anything else
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
-		return (1);
-	}
-	return (0);
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
 }
 
